38f69edd4ccca7de5c351778c7a4d141f87d9d02
prabhakanth <145905417+prabhakanth@users.noreply.github.com>
2023-12-20 17:58:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,13 @@
 terraform {
-  backend "s3" {
+  backend "s3"{
   bucket = "remotebackend-with-dynamodb"
   key = "global/backend/terraform.tfstate"
   region = "us-east-1"
   dynamodb_table = "remote-statelocking"
   encrypt = true
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
