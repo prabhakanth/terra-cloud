ae2a88b3e0b11a45482d03419c1f7d3426dacff1
prabhakanth <145905417+prabhakanth@users.noreply.github.com>
2023-12-20 17:05:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "aws" {
-    access_key = AKIA46DYQGRVER7VSMWB
-    region     = us-east-1
-    secret_key = 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
+    access_key = "AKIA46DYQGRVER7VSMWB"
+    region     = "us-east-1"
+    secret_key = "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"
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
