f6e5b12995cd312ba089e137909f1b45badd4fd3
prabhakanth <145905417+prabhakanth@users.noreply.github.com>
2023-12-20 16:44:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.0"
+        }
     }
-  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+    access_key = var.aws_access_key
+    region     = us-east-1
+    secret_key = var.aws_secret_key
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+    bucket = "lambda-source-code"
 }
