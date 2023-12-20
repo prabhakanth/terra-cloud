577cd57ce9743008cf79174acb2c02b572155a24
prabhakanth <145905417+prabhakanth@users.noreply.github.com>
2023-12-20 17:19:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,13 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.0"
-        }
-    }
+  backend "s3"{
+  bucket = "remotebackend-with-dynamodb"
+  key = "global/backend/terraform.tfstate"
+  region = "us-east-1"
+  dynamodb_table = "remote-statelocking"
+  encrypt = true
+  }
 }
 
 provider "aws" {
-    access_key = "AKIA46DYQGRVER7VSMWB"
-    region     = "us-east-1"
-    secret_key = "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"
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-    bucket = "lambda-source-code"
+  region = "us-east-1"
 }
